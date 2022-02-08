36f876174c136dc2f8abce7eae50fd42ed6014a4
coyote <megan.metelak@gmail.com>
2022-02-08 14:36:13 -0600

    connecting mysql database to node.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// import mysql2 file (installed to node_modules)
+const mysql = require('mysql2');
 // import express
 const express = require('express');
 // port designation and app expression
@@ -6,8 +8,26 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '100saladsareGreat!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// return all the data in the candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 // Default response for any other request (Not Found)
+// catchall route
 app.use((req, res) => {
     res.status(404).end();
 });
